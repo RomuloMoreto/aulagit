bb4dbf5f4ef6cab8a4a441c04b7246e06678887e
Rômulo Moreto <romulomoreto09@gmail.com>
2024-11-19 11:46:23 -0300

    Add files via upload

---
+++ jogo.historia/js/js.js
@@ -0,0 +1,13 @@
+
+document.getElementById('jogo').hidden = true;
+
+function iniciar(){
+    document.getElementById('jogo').hidden = false;
+    const arrastaveis = document.querySelectorAll('.arrastavel');
+    const dropzones = document.querySelectorAll('.dropzone');
+}
+
+const arrastaveis = document.querySelectorAll('.arrastavel');
+const dropzones = document.querySelectorAll('.dropzone');
+
+
